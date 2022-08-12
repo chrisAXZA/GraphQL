01d56dec6d5830b0d97752bf06e866907ea0e07c
chrisAXZA <nexter1@tutanota.com>
2022-08-12 16:36:04 +0300

    Implement type Query/User in type-defs

---
+++ Demo-Project-1/schema/type-defs.js
@@ -0,0 +1,19 @@
+// gql allows to write pure GQL code and automatically translate
+// the given code to JS compatible code
+import { gql } from 'apollo-server';
+
+// Every GraphQL schema starts with one specific type => Query, which
+// will hold all of the different queries (fields) which can be executed for 
+// the given API
+const typeDefs = gql`
+    type User {
+        name: String!
+        username: String!
+        age: Int!
+        nationality: String!
+    }
+
+    type Query {
+        users: [User!]!
+    }
+`
