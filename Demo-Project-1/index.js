2746c801a21ddfc1375dcc86ed41668f51c16a1d
chrisAXZA <nexter1@tutanota.com>
2022-08-12 16:18:38 +0300

    Add type-defs/resolvers files in schema folder

--- Demo-Project-1/index.js
+++ Demo-Project-1/index.js
@@ -1,10 +1,11 @@
-import { ApolloServer } from 'apollo-server'; 
+import { ApolloServer } from 'apollo-server';
 // SyntaxError: Cannot use import statement outside a module
 // const { ApolloServer } = require("apollo-server");
 
 // TypeDefs contains the list of all of the available data + queries for the given application
 // Resolvers will hold all of the functions (calls to API/Databases,...) 
-// that will interact with the data contained in TypeDefs 
+// that will interact with the data contained in TypeDefs
+// All of the information should be hold in the schema folder
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen()
