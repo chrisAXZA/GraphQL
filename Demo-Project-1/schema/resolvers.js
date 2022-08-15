536865ee80aa09ecad0eab72e1616de5a268146d
chrisAXZA <nexter1@tutanota.com>
2022-08-15 19:30:55 +0300

    Implement user query in type-defs.js

--- Demo-Project-1/schema/resolvers.js
+++ Demo-Project-1/schema/resolvers.js
@@ -6,6 +6,8 @@
 // the type-defs.js file, Query for this case which will contain all of
 // the resolver functions as the given sub-fields for each of the Query-type
 
+// import _ from 'lodash';
+
 import { UserList } from '../DummyData.js'
 
 const resolvers = {
@@ -15,6 +17,20 @@ const resolvers = {
         // retrieve data from the DB)
         users() {
             return UserList;
+        },
+        // Parent returns the value that has been resolved by a parent 
+        // in the chain of types
+        // user: (parent) => {
+        // Args is the second parameter, which is an object that contains whatever
+        // data is passed in the Query signature (in this case userId)
+        // user: (_, args) => {
+        user: (parent, args) => {
+            const userId = args.id;
+            return UserList.find((user) => user.id === Number(userId));
+            // lodash find will take as first argument the targetObject and as a second
+            // the search criteria (in this case userId)
+            // const user = _.find(UserList, { id: id });
+            // return user;
         },
     },
 };
--- Demo-Project-1/schema/type-defs.js
+++ Demo-Project-1/schema/type-defs.js
@@ -18,6 +18,7 @@ const typeDefs = gql`
 
     type Query {
         users: [User!]!
+        user(id: ID!): User!
     }
 
     enum Nationality {
