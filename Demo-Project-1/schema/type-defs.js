5a363d248c5c02e9ba4393c11a9bd4c8ab1b6a36
chrisAXZA <nexter1@tutanota.com>
2022-08-15 19:46:48 +0300

    Add Movie type in type-defs.js

--- Demo-Project-1/schema/type-defs.js
+++ Demo-Project-1/schema/type-defs.js
@@ -16,6 +16,13 @@ const typeDefs = gql`
         friends: [User]
     }
 
+    type Movie {
+        id: ID!
+        name: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!
+    }
+
     type Query {
         users: [User!]!
         user(id: ID!): User!
