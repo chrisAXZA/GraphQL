ee39ab0beac8f5cf6c659e1d2d6dec1ebb0b3379
chrisAXZA <nexter1@tutanota.com>
2022-08-31 18:42:37 +0300

    Implement queries all_users+all_movies

--- Demo-Project-1/client/src/DisplayData.js
+++ Demo-Project-1/client/src/DisplayData.js
@@ -8,6 +8,17 @@ const QUERY_ALL_USERS = gql`
             name
             age
             username
+            nationality
+        }
+    }
+`;
+
+const QUERY_ALL_MOVIES = gql`
+    query GetAllMovies {
+        movies {
+            name
+            yearOfPublication
+            isInTheaters
         }
     }
 `;
@@ -16,14 +27,53 @@ function DisplayData() {
     // useQuery hook allows ApolloClient to make call to the API. Fetches data from
     // API whenever the component renders.
     // gql statement is used in order to forumalate a GraphQL query
-    const { data } = useQuery(QUERY_ALL_USERS);
+    const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+    const { data: movieData, loading: loadingMovieData, error: errorMovieData } = useQuery(QUERY_ALL_MOVIES);
 
-    if (data){
-        console.log(data);
+    if (loading) {
+        return <h1>Data is being loaded...</h1>
+    }
+
+    // if (data) {
+    //     console.log(data);
+    // }
+
+    // if (error) {
+    //     console.log(error);
+    // }
+
+    if (movieData){
+        console.log(movieData);
     }
 
     return (
-        <div></div>
+        <div>
+            {data && data.users.map((user) => {
+                return (
+                    <>
+                        <div>
+                            <h1>Name: {user.name}</h1>
+                            <h1>Username: {user.username}</h1>
+                            <h1>Age: {user.age}</h1>
+                            <h1>Nationality: {user.nationality}</h1>
+                        </div>
+                        <hr></hr>
+                    </>
+                );
+            })}
+            {movieData && movieData.movies.map((movie) => {
+                return (
+                    <>
+                        <div>
+                            <h1>Movie Name: {movie.name}</h1>
+                            <h1>Release Year: {movie.yearOfPublication}</h1>
+                            <h1>Is in Theaters: {movie.isInTheaters ? 'Yes' : 'No'}</h1>
+                        </div>
+                        <hr></hr>
+                    </>
+                );
+            })}
+        </div>
     );
 }
 
