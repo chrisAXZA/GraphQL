effc90604abf5d8f5495452ab7d5c86237a0ae61
chrisAXZA <nexter1@tutanota.com>
2022-08-31 14:00:30 +0300

    Implement ApolloProvider in App.js

--- Demo-Project-1/client/src/App.js
+++ Demo-Project-1/client/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import {} from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 function App() {
+  // ApolloCient will be used by GraphQL to connect to an API
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    // ApolloClient library allows to cache info/data into your browser's cache
+    // when data is fetched and a given component is being changed, the page will contain
+    // latest data without the need to refresh the given page. If the data hasn't changed,
+    // data can be retrieved from cache instead of making a new call to the API, each time a new 
+    // component is being rendered on the browser page
+    uri: 'http://localhost:4000/graphql',
+    // uri specifies a link where a GraphQL API is running
+  });
+
   return (
-    <div className="App">
-      Hello GraphQL App
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        Hello GraphQL App
+      </div>
+    </ApolloProvider>
   );
 }
 
