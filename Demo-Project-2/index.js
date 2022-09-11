7e3f9ba768a31a880fd69e35a7e3ccdfdd89123e
chrisAXZA <nexter1@tutanota.com>
2022-09-11 17:39:26 +0300

    Add nodemon to package.json

--- Demo-Project-2/index.js
+++ Demo-Project-2/index.js
@@ -1,20 +1,48 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
-    # Query defines the way how the data is being fetched
     type Query {
-        # GraphQl either returns scalor/primitive or object type of data
         hello: String
+        # hello: [String!]!
     }
 `;
+
+// const typeDefs = gql`
+//     # Query defines the way how the data is being fetched
+//     type Query {
+//         # GraphQl either returns scalor/primitive or object type of data
+//         hello: String
+//         numberOfAnimals: Int
+//         price: Float
+//         isSold: Boolean
+//     }
+// `;
 
 const resolvers = {
     Query: {
         hello: () => {
             return 'Hello World';
+            // return ['Hello', 'World'];
         },
     },
 };
+
+// const resolvers = {
+//     Query: {
+//         hello: () => {
+//             return 'Hello World';
+//         },
+//         numberOfAnimals: () => {
+//             return 55;
+//         },
+//         price: () => {
+//             return 12.45;
+//         },
+//         isSold: () => {
+//             return false;
+//         },
+//     },
+// };
 
 const server = new ApolloServer({ typeDefs, resolvers, });
 
