0735d6b9db10c6417ae400624ad2f5a519886277
chrisAXZA <nexter1@tutanota.com>
2022-10-13 21:54:19 +0300

    Implement ClientType+RootQuery in schema.js

--- ProjectManager/server/schema/schema.js
+++ ProjectManager/server/schema/schema.js
@@ -1,3 +1,37 @@
-export const schema = {
+import {
+    GraphQLID,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+} from "graphql";
 
-};
+import { clients, projects } from "../sampleData.js";
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({ // function that returns object
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    }),
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: ({ // fields will relate to query functions
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // in the case of Mongoose, will take up Mongo function
+                return clients.find((client) => client.id === args.id);
+            },
+        },
+    }),
+});
+
+export const schema = new GraphQLSchema({
+    query: RootQuery,
+});
