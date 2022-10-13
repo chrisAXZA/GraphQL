812907cb0e96fd7df4580dfa57e10cfecb843d92
chrisAXZA <nexter1@tutanota.com>
2022-10-13 15:08:51 +0300

    Implement package.json scripts

--- ProjectManager/server/index.js
+++ ProjectManager/server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: '../.env', });
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.listen(port, console.log(`Server running on port >>> ${port}`));
