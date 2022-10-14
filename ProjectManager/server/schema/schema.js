5b19a86a1c38d3b33e74cfe386cfafa8cafc8d8c
chrisAXZA <nexter1@tutanota.com>
2022-10-14 11:43:29 +0300

    Implement ProjectType+project/projects queries

--- ProjectManager/server/schema/schema.js
+++ ProjectManager/server/schema/schema.js
@@ -19,15 +19,21 @@ const ClientType = new GraphQLObjectType({
     }),
 });
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        clientId: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+    }),
+});
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: ({ // fields will relate to query functions
-        clients: {
-            type: new GraphQLList(ClientType),
-            resolve(parent, args) {
-                return clients;
-            },
-        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
@@ -36,6 +42,25 @@ const RootQuery = new GraphQLObjectType({
                 return clients.find((client) => client.id === args.id);
             },
         },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients;
+            },
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return projects.find((project) => project.id === args.id);
+            },
+        },
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            },
+        }
     }),
 });
 
