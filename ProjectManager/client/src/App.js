c0a1c4d0d56cb11445e4f4af2268009e124aaff6
chrisAXZA <nexter1@tutanota.com>
2022-10-21 22:02:16 +0300

    Implement Header.jsx+import in App.js

--- ProjectManager/client/src/App.js
+++ ProjectManager/client/src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
+
+import Header from "./components/Header.jsx";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+
+            <div className="container">
+                <h1>Hello World</h1>
+            </div>
+        </>
+    );
 }
 
 export default App;
