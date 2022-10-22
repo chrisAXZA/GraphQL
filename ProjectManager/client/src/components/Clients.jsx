3815a3158189971c784a341b5db31e40f01761fe
chrisAXZA <nexter1@tutanota.com>
2022-10-22 19:16:04 +0300

    Implement ClientRow.jsx+import in Client.jsx

--- ProjectManager/client/src/components/Clients.jsx
+++ ProjectManager/client/src/components/Clients.jsx
@@ -2,6 +2,8 @@ import React from "react";
 // gql creates query, useQuery allows access to data (+loadingState, any errors) in given component
 import { gql, useQuery } from '@apollo/client';
 
+import ClientRow from "./ClientRow.jsx";
+
 const GET_CLIENTS = gql`
     query getClients {
         clients {
@@ -19,19 +21,37 @@ export default function Clients() {
 
     if (loading) {
         return (
-            <p style="font: bold; color: green;">Loading...</p>
+            <p style={{ fontSize: "24px", color: "green" }}>Loading...</p>
         );
     }
 
     if (error) {
         return (
-            <p style="font: bold; color: red;">Some error occurred!</p>
+            <p style={{ fontSize: "24px", color: "red" }}>Some error occurred!</p>
         );
     }
 
     return (
         <>
-            {!loading && !error && <h1>Clients</h1>}
+            {/* {!loading && !error && <h1 style={{color: "gray", background: "pink"}}>Clients</h1>} */}
+            {!loading && !error && (
+                <table className="table table-hover mt-3">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            {/* delete Button */}
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.clients.map((client => {
+                            return <ClientRow key={client.id} client={client} />
+                        }))}
+                    </tbody>
+                </table>
+            )}
         </>
     );
 }
