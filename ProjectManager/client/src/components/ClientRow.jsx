7e92b19ac3b3eccb2e9d7389fc606bc7d76b914d
chrisAXZA <nexter1@tutanota.com>
2022-10-24 19:54:16 +0300

    Implement refetchQueries+update cache in ClientRow

--- ProjectManager/client/src/components/ClientRow.jsx
+++ ProjectManager/client/src/components/ClientRow.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import { FaTrash } from 'react-icons/fa';
 import { useMutation } from "@apollo/client";
 
+import { GET_CLIENTS } from "../queries/clientQueries.js";
 import { DELETE_CLIENT } from "../mutations/clientMutations.js";
 
 export default function ClientRow({ client }) {
-    console.log(client, 'here');
+    // console.log(client);
 
     const [deleteClient, { loading, error, data }] = useMutation(DELETE_CLIENT, {
         variables: { clientId: client.id },
-        // onError(err) {
-        //     console.log(err);
-        // },
+        // refetchQueries: [{ query: GET_CLIENTS }], // increases low performance when applied to too many queries
+        update(cache, { data: { deleteClient } }) {
+            // const {clients} receives clientData from the cache
+            // cache will update data and filter out client without making a new request (more performant)
+            const { clients } = cache.readQuery({ query: GET_CLIENTS });
+
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: { clients: clients.filter((client) => client.id !== deleteClient.id) }, 
+                // filters out the deleting client's id (ClientType!)
+            });
+        },
     });
 
     // if (error) {
