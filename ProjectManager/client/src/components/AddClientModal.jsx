b4920d352bc4ae3b97d8927b9703e207f7ba2026
chrisAXZA <nexter1@tutanota.com>
2022-10-25 17:14:57 +0300

    Implement submitForm in AddClientModal.jsx

--- ProjectManager/client/src/components/AddClientModal.jsx
+++ ProjectManager/client/src/components/AddClientModal.jsx
@@ -2,28 +2,44 @@ import React, { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 
-export default function () {
+export default function AddClientModal() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(name, email, phone);
+    };
+
     return (
         <>
             {/* Button trigger modal */}
             <button
                 type="button"
-                className="btn btn-primary"
+                className="btn btn-secondary"
                 data-bs-toggle="modal"
-                data-bs-target="#exampleModal">
-                Launch demo modal
+                data-bs-target="#addClientModal">
+                <div className="d-flex align-items center">
+                    <FaUser className="icon" />
+                    <div>Add Client</div>
+                </div>
             </button>
 
             {/* Modal */}
             <div
                 className="modal fade"
-                id="exampleModal"
-                aria-labelledby="exampleModalLabel"
+                id="addClientModal"
+                aria-labelledby="addClientModalLabel"
                 aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <h1
+                                className="modal-title fs-5"
+                                id="addClientModalLabel">
+                                Add Client
+                            </h1>
                             <button
                                 type="button"
                                 className="btn-close"
@@ -32,9 +48,44 @@ export default function () {
                             </button>
                         </div>
                         <div className="modal-body">
-                            ...
+                            <form onSubmit={onSubmit}>
+                                <div className="mb-3">
+                                    <label className="form-label">Name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)} />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Email</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)} />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Phone</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="phone"
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)} />
+                                </div>
+                                <button
+                                    className="btn btn-secondary"
+                                    type="submit"
+                                    data-bs-dismiss="modal">
+                                    {/* data-dismiss will close modal window after data has been submitted */}
+                                    Submit ClientData
+                                </button>
+                            </form>
                         </div>
-                        <div className="modal-footer">
+                        {/* <div className="modal-footer">
                             <button
                                 type="button"
                                 className="btn btn-secondary"
@@ -44,13 +95,12 @@ export default function () {
                             <button
                                 type="button"
                                 className="btn btn-primary">
-                                Save changes
+                                Add Client
                             </button>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
-
         </>
     );
 }
