a70efef6d481f08fd78702e513dcade5a7f8b9e2
chrisAXZA <nexter1@tutanota.com>
2022-10-29 18:12:48 +0300

    Implement refetchQueries for GET_PROJECTS

--- ProjectManager/client/src/components/ClientRow.jsx
+++ ProjectManager/client/src/components/ClientRow.jsx
@@ -3,6 +3,7 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from "@apollo/client";
 
 import { GET_CLIENTS } from "../queries/clientQueries.js";
+import {GET_PROJECTS } from "../queries/projectQueries.js";
 import { DELETE_CLIENT } from "../mutations/clientMutations.js";
 
 export default function ClientRow({ client }) {
@@ -11,18 +12,18 @@ export default function ClientRow({ client }) {
     // const [deleteClient, { loading, error, data }] = useMutation(DELETE_CLIENT, {
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         variables: { clientId: client.id },
-        // refetchQueries: [{ query: GET_CLIENTS }], // increases low performance when applied to too many queries
-        update(cache, { data: { deleteClient } }) {
-            // const {clients} receives clientData from the cache
-            // cache will update data and filter out client without making a new request (more performant)
-            const { clients } = cache.readQuery({ query: GET_CLIENTS });
+        refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }], // increases low performance when applied to too many queries
+        // update(cache, { data: { deleteClient } }) {
+        //     // const {clients} receives clientData from the cache
+        //     // cache will update data and filter out client without making a new request (more performant)
+        //     const { clients } = cache.readQuery({ query: GET_CLIENTS });
 
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: { clients: clients.filter((client) => client.id !== deleteClient.id) },
-                // filters out the deleting client's id (ClientType!)
-            });
-        },
+        //     cache.writeQuery({
+        //         query: GET_CLIENTS,
+        //         data: { clients: clients.filter((client) => client.id !== deleteClient.id) },
+        //         // filters out the deleting client's id (ClientType!)
+        //     });
+        // },
     });
 
     // if (error) {
